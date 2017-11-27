037e3531339896d4f22fb62964b7d858f2bc448f
Marcus Pedro <marcus_mp123@hotmail.com>
2017-11-27 20:44:07 -0200

    quarto teste

---
+++ consulta2.php
@@ -0,0 +1,11 @@
+If ( primeira condição){
+Primeira ação,
+} elseif( segunda condição) {
+Segunda ação.
+} elseif ( terceira condição) {
+	termina ação
+} elseif ( quantas conições precisar) {
+	Ação a ser executada
+} else {
+	ação padrão
+}
